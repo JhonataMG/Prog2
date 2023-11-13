2c00698bec4a53484792e44d57e4b602f914f814
aluno <jhonatamg1997@gmail.com>
2023-11-13 11:18:35 -0300

    Começo da função

---
+++ lista-01/ord_arq_gran.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<limits.h>
+
+
+
+
+int main(void){
+    int qts_arq = 100;
+    int tam = n;
+    int total = 0;
+    File *arq
+    printf("Maior número int: %d\n", INT_MAX);
+    return 0;
+}
